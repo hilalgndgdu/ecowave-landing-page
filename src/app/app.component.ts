38488807a4e293063096551575ebde470acd2e2f
hilalgndgdu <154067326+hilalgndgdu@users.noreply.github.com>
2024-06-04 18:34:41 +0300

    component and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/component/header/header/header.component';
+import { FooterComponent } from './core/component/footer/footer/footer.component';
+import { MainContentComponent } from './core/component/main-content/main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , HeaderComponent, FooterComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
